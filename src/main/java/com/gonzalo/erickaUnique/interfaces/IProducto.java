933248bd5615a729ad7d738457f690ec0b0dbc21
GonzaloJunco <gonza2609@gmail.com>
2020-06-09 03:04:18 -0500

    Procfile

--- src/main/java/com/gonzalo/erickaUnique/interfaces/IProducto.java
+++ src/main/java/com/gonzalo/erickaUnique/interfaces/IProducto.java
@@ -1,11 +1,13 @@
 package com.gonzalo.erickaUnique.interfaces;
 
+import org.hibernate.cfg.JPAIndexHolder;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gonzalo.erickaUnique.model.Producto;
 
 @Repository
-public interface IProducto extends CrudRepository<Producto, Integer> {
+public interface IProducto extends JpaRepository<Producto, Integer> {
 
 }
